758451855f98a6a063bcf0a4b44b705ec9540e62
Kelsey Richards <kelseyrichards@me.com>
2025-08-27 14:32:37 -0600

    Formatting only

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,38 @@
 //API OpenWeather
-const apiKey = '81169cf5652a98069c5713e74765afc5';
+const apiKey = "81169cf5652a98069c5713e74765afc5";
 //DOM Elements
-const cityInputElement = document.getElementById('citySearch');
-const cityDisplayElement = document.getElementById('cityDisplay');
-const currentWeatherDescription = document.getElementById('currentWeatherDescription');
-const currentWeatherIcon = document.getElementById('currentWeatherIcon');
-const currentWeatherNumber = document.getElementById('currentWeatherNumber');
+const cityInputElement = document.getElementById("citySearch");
+const cityDisplayElement = document.getElementById("cityDisplay");
+const currentWeatherDescription = document.getElementById(
+	"currentWeatherDescription"
+);
+const currentWeatherIcon = document.getElementById("currentWeatherIcon");
+const currentWeatherNumber = document.getElementById("currentWeatherNumber");
 
-
-
-
-cityInputElement.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const cityInput = cityInputElement.value;
-  console.log('cityInput:', cityInput);
-  async (params) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`);
-    const data = await response.json();
-    console.log(data);
-
-  }
-  
+cityInputElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const cityInput = cityInputElement.value;
+	console.log("cityInput:", cityInput);
+	async (params) => {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`
+		);
+		const data = await response.json();
+		console.log(data);
+	};
 });
 
 //Temperature unit toggle
-const tempUnitToggle = document.getElementById('switchCheckDefault');
-
-
+const tempUnitToggle = document.getElementById("switchCheckDefault");
 
 function dynamicTextSize() {
-  const textLength = cityInput.length;
-  // Adjust font size based on textLength
-  if (textLength < 10) {
-    cityDisplayElement.style.fontSize = '42px';
-  } else if (textLength >= 10 && textLength < 20) {
-    cityDisplayElement.style.fontSize = '32px';
-  } else {
-    cityDisplayElement.style.fontSize = '22px';
-  }
+	const textLength = cityInput.length;
+	// Adjust font size based on textLength
+	if (textLength < 10) {
+		cityDisplayElement.style.fontSize = "42px";
+	} else if (textLength >= 10 && textLength < 20) {
+		cityDisplayElement.style.fontSize = "32px";
+	} else {
+		cityDisplayElement.style.fontSize = "22px";
+	}
 }
